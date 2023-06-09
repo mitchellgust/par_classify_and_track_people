9e9ab3ebbd0ca20274b03090fe92401dad9aa42c
Alec Sathiyamoorthy <s3937453@student.rmit.edu.au>
2023-06-09 20:53:17 +1000

    Send pointcloud data direct when not none.

--- noetic_workspace/src/first_pkg/scripts/velodyne_interpret.py
+++ noetic_workspace/src/first_pkg/scripts/velodyne_interpret.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
+
 import rospy
+import open3d as o3d
+import numpy as np
+import pclpy
+from pclpy import pcl
 from datetime import datetime
 from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2, PointField
@@ -16,6 +21,51 @@ class InterpretVelodyne:
         self.last_update = 0
         self.update_frequency_seconds = 10
         self.filtered_point_cloud_publisher = rospy.Publisher('filtered_cloud', PointCloud2, queue_size=10)
+
+    # # Techniques taken from here: https://betterprogramming.pub/point-cloud-filtering-in-python-e8a06bbbcee5
+    # def remove_noise_from_point_cloud_2(self, filtered_cloud):
+        
+    #     xyz = np.array([[0,0,0]])
+    #     rgb = np.array([[0,0,0]])
+
+    #     # Data conversion from sensor_msgs pointcloud to o3d pointcloud: https://answers.ros.org/question/255351/how-o-save-a-pointcloud2-data-in-python/
+    #     for x in filtered_cloud:
+    #         test = x[3]
+
+    #         # cast float32 to int so that bitwise operations are possible
+    #         s = struct.pack('>f' ,test)
+    #         i = struct.unpack('>l',s)[0]
+            
+    #         # you can get back the float value by the inverse operations
+    #         pack = ctypes.c_uint32(i).value
+    #         r = (pack & 0x00FF0000)>> 16
+    #         g = (pack & 0x0000FF00)>> 8
+    #         b = (pack & 0x000000FF)
+            
+    #         # prints r,g,b values in the 0-255 range
+    #                     # x,y,z can be retrieved from the x[0],x[1],x[2]
+    #         xyz = np.append(xyz,[[x[0],x[1],x[2]]], axis = 0)
+    #         rgb = np.append(rgb,[[r,g,b]], axis = 0)
+        
+    #     out_pcd = o3d.geometry.PointCloud()
+    #     out.pcd.points = o3d.utility.Vector3dVector(xyz)
+    #     out_pcd.colors = o3d.utility.Vector3dVector(rgb)
+
+    #     # Create bounding box:
+    #     bounds = [[-math.inf, math.inf], [-math.inf, math.inf], [0.8, 2]]  # set the bounds
+    #     bounding_box_points = list(itertools.product(*bounds))  # create limit points
+    #     bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(
+    #         o3d.utility.Vector3dVector(bounding_box_points))  # create bounding box object
+
+    #     # Crop the point cloud using the bounding box:
+    #     pcd_croped = pcd.crop(bounding_box)
+        
+    #     return out_pcd.points
+
+        
+
+
+
 
     def interpret_point_cloud_2(self, point_cloud_data : PointCloud2):
         
@@ -34,10 +84,21 @@ class InterpretVelodyne:
             
             # Recreate point cloud.
             # header = Header()
-            header = point_cloud_data.header                        # Field names included in list.
-            filtered_point_cloud = point_cloud2.create_cloud_xyz32(header, filtered_point_cloud_list)
+            # header = point_cloud_data.header                        # Field names included in list.
+            # filtered_point_cloud = point_cloud2.create_cloud_xyz32(header, filtered_point_cloud_list)
             
-            self.filtered_point_cloud_publisher.publish(filtered_point_cloud)
+            # self.filtered_point_cloud_publisher.publish(filtered_point_cloud)
+
+            # std::shared_ptr<std::vector<int>> indices(new std::vector<int>);
+            # pcl::removeNaNFromPointCloud(*source_cloud, *indices);
+            # pcl::ExtractIndices<pcl::PointXYZ> extract;
+            # extract.setInputCloud(source_cloud);
+            # extract.setIndices(indices);
+            # extract.setNegative(true);
+            # extract.filter(*source_cloud);
+
+            # out = self.remove_noise_from_point_cloud_2(filtered_cloud)
+            self.filtered_point_cloud_publisher.publish(point_cloud_data)
 
     """
     Start subsribers.
