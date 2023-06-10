2e25332ac417867820d63036a53cd77b9d6a3764
Mitchell Gust <s3782095@student.rmit.edu.au>
2023-06-10 14:08:39 +1000

    Output Difference of Current and Previous Position

--- scripts/track_object.py
+++ scripts/track_object.py
@@ -4,171 +4,92 @@ import rospy
 
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose
+from visualization_msgs import MarkerArray
 from datetime import datetime
 
-# TODO: Not tested AT ALL. Just started planning the structure of the script
-
 # Stores Data based on each Object Identified through Topic
-IdentifiedObject: identified_objects = []
-
-# Holds Currently Tracked Object ID
-tracked_object_id = None 
-
-# Num. of Times an Object is Seen before Average is Produced
-# - Use this to be confident on its movement.
-POSITIONS_BEFORE_AVERAGE_IS_MADE = 3
+identified_objects = []
 
 class IdentifiedObject:    
-    def __init__(self, object_id, position):
-        self.object_id = object_id
+    def __init__(self, object_id: int, position: Pose):
+        self.object_id: int = object_id
 
-        # List of Tracked Positions - (Pose, timestamp, probability)
+        # List of Tracked Positions - (Pose, timestamp)
         self.tracked_positions = []
         self.tracked_positions.append(position)
 
-        # Track Number of Tracked Positions
-        self.num_tracked_positions = len(tracked_positions)
+        # Store Number of Tracked Positions
+        self.num_tracked_positions: int = len(self.tracked_positions)
 
-        # Previous Average of x Most Recent Position - (Pose)
-        self.previous_position_average = None
+        self.last_position: Pose = None
+        self.current_position: Pose = Pose
 
-        # Average of x Most Recent Positions - (Pose)
-        self.latest_position_average = None # Produce Average on Every 5 Reading
-
-    def get_object_id(self):
+    def get_object_id(self) -> int:
         return self.object_id
     
-    def get_latest_position_average(self):
-        return self.latest_position_average
-    
-    def get_tracked_positions(self):
+    def get_tracked_positions(self) -> list:
         return self.tracked_positions
     
-    def get_num_tracked_positions(self):
+    def get_num_tracked_positions(self) -> int:
         return self.num_tracked_positions
     
     def add_tracked_position(self, tracked_position):
         self.num_tracked_positions += 1
         self.tracked_positions.append(tracked_position)
-    
-    def calc_average_from_tracked_positions(self, object_id):
-        x_average = 0
-        x_values = []
+        self.last_position = tracked_position
 
-        y_average = 0
-        y_values = []
-
-        z_average = 0
-        z_values = []
-
-        # Sort Tracked Positions by Timestamp
-        self.tracked_positions = sorted(self.tracked_positions, key=self.get_timestamp_of_tracked_position)
-
-        for index, position in enumerate(self.tracked_positions):
-            x_values.append(position.x)
-            y_values.append(position.y)
-            z_values.append(position.z)
-
-            # Only Get x Most Recent Positions
-            if index + 1 == POSITIONS_BEFORE_AVERAGE_IS_MADE:
-                break
-        
-        # Get the average of the x Most Recent Position Values (x,y,z)
-        x_average = sum(x_values) / len(x_values)
-        y_average = sum(y_values) / len(y_values)
-        z_average = sum(z_values) / len(z_values)
-
-        self.previous_position_average = self.latest_position_average
-
-        self.latest_position_average = Pose(x_average, y_average, z_average)
-
-    def get_timestamp_of_tracked_position(self, tracked_position):
+    def get_timestamp_of_tracked_position(self, tracked_position) -> datetime:
         return tracked_position.timestamp
 
 class TrackedPosition:
-    def __init__(self, Pose: position, datetime: timestamp, int: confidence):
+    def __init__(self, position: Pose, timestamp: datetime):
         self.position = position
         self.timestamp = timestamp
-        self.confidence = confidence
 
-def found_object_callback(found_object):
+    def get_position(self):
+        return self.position
+    
+    def get_timestamp(self):
+        return self.timestamp
 
-    # Get objectID, trackedPosition
-    # If Exists
-    if identified_objects.contains(found_object.object_id):
-        # TODO: Update Object based on Object_id
-        identified_objects.update()
+def viz_callback(markerArray: MarkerArray):
+    # For each Marker in MarkerArray
+    for marker in markerArray.markers:
+        # If Marker is a New Object
+        if marker.id not in identified_objects:
+            # Create New Object
+            identified_objects[marker.id] = IdentifiedObject(marker.id, marker.pose)
+        # Else, Update Object
+        else:
+            identified_objects[marker.id].add_tracked_position(marker.pose)
 
-    # If Doesnt Exists
-    else:
-        # Create New Object
-        identified_objects.append(IdentifiedObject(found_object.object_id, tracked_position))
+def output_identified_objects():
+    # For each Identified Object
+    for object_id in identified_objects:
+        rospy.loginfo("Object ID: " + object_id)
+        # rospy.loginfo("Tracked Positions: " + identified_objects[object_id].get_tracked_positions())
+        # rospy.loginfo("Num. of Tracked Positions: " + identified_objects[object_id].get_num_tracked_positions())
+        # rospy.loginfo("Last Position: " + identified_objects[object_id].get_last_position())
 
-
-    # If NumTrackPositions > 5
-    # Calculate latest_position_average
-    # Update latest_position average value in identified_objects list
-
-    # If latest tracked position of identified_object_id is not within 10 seconds of current time, clear identified_object_id
-
-    # If identified_object_id is -1
-    # Set identified_object_id to objectID
-
-
-# Use tracked_object_id to determine which object to track
-# Determine if object is going left or right
-# Perform Rotation
-# def follow_object():
+        # Output difference between last and current position
+        rospy.loginfo("Difference between last and current position: " + identified_objects[object_id].get_last_position() - identified_objects[object_id].get_current_position())
 
 
-def rotate_left():
-    # Rotate Left
-    cmd.angular.z = speed
-    pub_drive.publish(cmd)
-    rate.sleep()
-
-    # Stop Robot
-    cmd.angular.z = 0
-    pub_drive.publish(cmd)
-
-def rotate_right():
-    # Rotate Right
-    cmd.angular.z = -speed
-    pub_drive.publish(cmd)
-    rate.sleep()
-
-    # Stop Robot
-    cmd.angular.z = 0
-    pub_drive.publish(cmd)
-     
-
-# Short ROS Node method
 if __name__ == '__main__':
     try:
-        rospy.init_node('track_object_by_rotating', anonymous=True)
-       
-        # Initiate Twist parameters
-        cmd = Twist()
-        cmd.linear.x = 0
-        cmd.linear.y = 0
-        cmd.linear.z = 0
-        cmd.angular.x = 0
-        cmd.angular.y = 0
-        cmd.angular.z = 0
+        rospy.init_node('track_viz_data', anonymous=True)
 
         # Drive publisher
         pub_drive = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
-        # Paramaters
-        speed = rospy.get_param("~speed", 0.5)
-        max_duration = rospy.get_param("~duration", 3)
-        t_start = rospy.Time.now()
-        stop = False
+        # Subscribe to MarkerArray
+        rospy.Subscriber("/viz", MarkerArray, viz_callback)
 
-        rate = rospy.Rate(10)
-
-        # Subscribe to Data Passed from Lidar Object Detection Node
-        rospy.Subscriber("/identified_objects", FoundObject, found_object_callback)
+        # Output the Identified Objects and their Tracked Positions every 5 seconds
+        while not rospy.is_shutdown():
+            output_identified_objects()
+            rospy.sleep(5)
 
     except rospy.ROSInterruptException:
         pass
+        
