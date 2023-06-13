9108b96285436f6fc83c7dc2748b26ac8092d186
Gao Fan <s3734811@student.rmit.edu.au>
2023-06-13 16:17:14 +1000

    add zed file

--- tracking/noetic_workspace/src/first_pkg/scripts/zed.py
+++ tracking/noetic_workspace/src/first_pkg/scripts/zed.py
@@ -1,12 +1,43 @@
 import rospy
 from zed_interfaces.msg import ObjectsStamped
+from visualization_msgs.msg import Marker
 
 def callback(data):
-    rospy.loginfo(data)
+    for obj in data.objects:
+        label = obj.label
+        label_id = obj.label_id
+        sublabel = obj.sublabel
+        confidence = obj.confidence
+        position = obj.position
+
+        rospy.loginfo("Object: Label=%s, Label ID=%d, Sublabel=%s, Confidence=%.2f, Position=%.2f, %.2f, %.2f",
+                      label, label_id, sublabel, confidence, position[0], position[1], position[2])
+
+        # Create a marker for the object
+        marker = Marker()
+        marker.header.frame_id = "map"  # Set the frame ID of the marker to match your map's frame ID
+        marker.type = Marker.SPHERE  # Set the marker type to represent a sphere
+        marker.pose.position.x = position[0]  # Set the position of the marker
+        marker.pose.position.y = position[1]
+        marker.pose.position.z = position[2]
+        marker.scale.x = 0.1  # Set the size of the marker
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.a = 1.0  # Set the transparency of the marker
+        marker.color.r = 1.0  # Set the color of the marker (e.g., red)
+        marker.color.g = 0.0
+        marker.color.b = 0.0
+
+        # Publish the marker
+        marker_pub.publish(marker)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("zed2/zed_node/obj_det/objects", ObjectsStamped, callback)
+    rospy.Subscriber("zed2i/zed_node/obj_det/objects", ObjectsStamped, callback)
+
+    # Create a publisher for visualization markers
+    marker_pub = rospy.Publisher("visualization_marker", Marker, queue_size=10)
+
     rospy.spin()
 
 if __name__ == '__main__':
