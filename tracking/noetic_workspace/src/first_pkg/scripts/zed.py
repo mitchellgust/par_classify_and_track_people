03f205129b24acd87ff563da92469bb847d2ff9a
Gao Fan <s3734811@student.rmit.edu.au>
2023-06-13 16:42:04 +1000

    add ori for mark

--- tracking/noetic_workspace/src/first_pkg/scripts/zed.py
+++ tracking/noetic_workspace/src/first_pkg/scripts/zed.py
@@ -1,6 +1,7 @@
 import rospy
 from zed_interfaces.msg import ObjectsStamped
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Quaternion
 
 # Define marker publisher as a global variable
 marker_pub = None
@@ -16,13 +17,15 @@ def callback(data):
         rospy.loginfo("Object: Label=%s, Label ID=%d, Sublabel=%s, Confidence=%.2f, Position=%.2f, %.2f, %.2f",
                       label, label_id, sublabel, confidence, position[0], position[1], position[2])
 
-        # Create a marker for the object
+        # Create a marker for each object
         marker = Marker()
         marker.header.frame_id = "map"  # Set the frame ID of the marker to match your map's frame ID
+        marker.id = label_id  # Assign a unique marker ID based on the label ID
         marker.type = Marker.SPHERE  # Set the marker type to represent a sphere
         marker.pose.position.x = position[0]  # Set the position of the marker
         marker.pose.position.y = position[1]
         marker.pose.position.z = position[2]
+        marker.pose.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)  # Initialize quaternion to identity orientation
         marker.scale.x = 0.1  # Set the size of the marker
         marker.scale.y = 0.1
         marker.scale.z = 0.1
@@ -38,7 +41,7 @@ def listener():
     global marker_pub  # Declare marker_pub as a global variable
 
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("zed2/zed_node/obj_det/objects", ObjectsStamped, callback)
+    rospy.Subscriber("zed2i/zed_node/obj_det/objects", ObjectsStamped, callback)
 
     # Create a publisher for visualization markers
     marker_pub = rospy.Publisher("visualization_marker", Marker, queue_size=10)
