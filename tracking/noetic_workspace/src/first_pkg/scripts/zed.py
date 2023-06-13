e1fd7b01b340cbaf35890e5a162dec01e617404b
Gao Fan <s3734811@student.rmit.edu.au>
2023-06-13 15:22:42 +1000

    change to get basic info

--- tracking/noetic_workspace/src/first_pkg/scripts/zed.py
+++ tracking/noetic_workspace/src/first_pkg/scripts/zed.py
@@ -2,12 +2,7 @@ import rospy
 from zed_interfaces.msg import ObjectsStamped
 
 def callback(data):
-    # This is where you process the ObjectsStamped data.
-    # For example, you might print the 3D position of the first detected person:
-    for obj in data.objects:  
-        if obj.label == "person":  # if this object is a person
-            rospy.loginfo(f"Detected a person at position: {obj.position}")
-            break  # stop after finding the first person
+    rospy.loginfo(data)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
