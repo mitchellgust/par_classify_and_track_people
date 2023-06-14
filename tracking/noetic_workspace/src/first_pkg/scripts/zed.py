cdee5d5e5218f49170297f4d14a5937780becf33
Gao Fan <s3734811@student.rmit.edu.au>
2023-06-14 13:44:54 +1000

    change update function

--- tracking/noetic_workspace/src/first_pkg/scripts/zed.py
+++ tracking/noetic_workspace/src/first_pkg/scripts/zed.py
@@ -3,58 +3,41 @@ from zed_interfaces.msg import ObjectsStamped
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Quaternion
 from std_msgs.msg import ColorRGBA
-import time
 
 # Define marker publisher as a global variable
 marker_pub = None
-# Dictionary to store the marker positions and timestamps for each label ID
-marker_positions = {}
-
-# Time (in seconds) after which a marker will be considered expired
-MARKER_EXPIRATION_TIME = 5.0
+# Dictionary to store the marker positions and tracking states for each label ID
+marker_data = {}
 
 
 def callback(data):
-    # Get the current time
-    current_time = rospy.Time.now()
-
-    # Remove expired markers
-    remove_expired_markers(current_time)
-
     for obj in data.objects:
         label = obj.label
         label_id = obj.label_id
         sublabel = obj.sublabel
         confidence = obj.confidence
         position = obj.position
+        tracking_state = obj.tracking_state
 
         if label == "Person" and confidence >= 60.0:
-            # Update the marker position and timestamp for the current object
-            marker_positions[label_id] = (position, current_time)
+            if tracking_state == 0:
+                # Remove the marker if the object's tracking state is 0
+                remove_marker(label_id)
+            else:
+                # Update the marker position and tracking state for the current object
+                marker_data[label_id] = (position, tracking_state)
 
     # Publish the markers
     publish_markers()
 
 
-def remove_expired_markers(current_time):
-    # List to store the IDs of the markers that need to be removed
-    markers_to_remove = []
-
-    for label_id, (_, marker_time) in marker_positions.items():
-        # Calculate the elapsed time since the marker was last updated
-        elapsed_time = (current_time - marker_time).to_sec()
-
-        if elapsed_time >= MARKER_EXPIRATION_TIME:
-            # Add the expired marker ID to the list
-            markers_to_remove.append(label_id)
-
-    # Remove the expired markers from the marker_positions dictionary
-    for label_id in markers_to_remove:
-        marker_positions.pop(label_id)
+def remove_marker(label_id):
+    # Remove the marker from the marker_data dictionary
+    marker_data.pop(label_id)
 
 
 def publish_markers():
-    for label_id, (position, _) in marker_positions.items():
+    for label_id, (position, _) in marker_data.items():
         # Create a marker for each object
         marker = Marker()
         marker.header.frame_id = "map"  # Set the frame ID of the marker to match your map's frame ID
