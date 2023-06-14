a4cd478cb5b621dd4f51f3e2822e3404a94bb1da
Gao Fan <s3734811@student.rmit.edu.au>
2023-06-14 14:55:24 +1000

    add rotate function test

--- tracking/noetic_workspace/src/first_pkg/scripts/zed.py
+++ tracking/noetic_workspace/src/first_pkg/scripts/zed.py
@@ -1,7 +1,8 @@
 import rospy
 from zed_interfaces.msg import ObjectsStamped
 from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Twist
+import math
 
 # Define marker publisher as a global variable
 marker_pub = None
@@ -9,6 +10,9 @@ marker_pub = None
 marker_positions = {}
 # Keep track of the last update time for each marker
 marker_times = {}
+
+# Drive publisher
+pub_drive = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
 def callback(data):
     current_time = rospy.get_rostime()
@@ -23,8 +27,8 @@ def callback(data):
         tracking_state = obj.tracking_state
 
         if label == "Person" and confidence >= 60.0:
-            rospy.loginfo("Object: Label=%s, Label ID=%d, Sublabel=%s, Confidence=%.2f, Position=%.2f, %.2f, %.2f, tracking=%d",
-                          label, label_id, sublabel, confidence, position[0], position[1], position[2], tracking_state)
+            # rospy.loginfo("Object: Label=%s, Label ID=%d, Sublabel=%s, Confidence=%.2f, Position=%.2f, %.2f, %.2f, tracking=%d",
+            #               label, label_id, sublabel, confidence, position[0], position[1], position[2], tracking_state)
 
             # Update the marker position for the current object
             marker_positions[label_id] = position
@@ -40,6 +44,36 @@ def callback(data):
 
     # Publish the markers
     publish_markers()
+    rotate_to_closest_marker()
+
+def rotate_to_closest_marker():
+    closest_marker_id = find_closest_marker()
+    if closest_marker_id is not None:
+        angle = compute_angle_to_marker(marker_positions[closest_marker_id])
+
+        # Now you can use this angle to drive your robot
+        cmd = Twist()
+        # Define maximum rotation speed
+        MAX_ROT_SPEED = 1.0  # radians/second
+
+        # Scale angle to limit maximum rotation speed
+        cmd.angular.z = max(min(angle, MAX_ROT_SPEED), -MAX_ROT_SPEED)
+        pub_drive.publish(cmd)
+
+def compute_angle_to_marker(position):
+    return math.atan2(position[1], position[0])
+
+def find_closest_marker():
+    closest_marker_id = None
+    closest_marker_distance = float('inf')
+
+    for marker_id, position in marker_positions.items():
+        distance = math.hypot(position[0], position[1])  # Compute distance to marker
+        if distance < closest_marker_distance:
+            closest_marker_distance = distance
+            closest_marker_id = marker_id
+
+    return closest_marker_id
 
 def publish_markers():
     for label_id, position in marker_positions.items():
